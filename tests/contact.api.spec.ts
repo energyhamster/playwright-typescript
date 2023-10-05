2d80d7696124f9dd38f867d8e21183de694da3df
Vladyslav Opanasenko <vla.opanasenko@gmail.com>
2023-10-05 16:53:56 +0300

    added api controller and modified contact page test

--- tests/contact.api.spec.ts
+++ tests/contact.api.spec.ts
@@ -1,18 +1,13 @@
 import { test, expect, APIRequestContext, APIResponse } from "@playwright/test";
 import ContactPage from "../pages/contact.page.ts";
+import APIController from "../controller/api.controller.ts";
 
 let contactPage: ContactPage;
-let fakerApi: APIRequestContext;
 let person: APIResponse;
 
 test.beforeAll(async ({ playwright }) => {
-    fakerApi = await playwright.request.newContext({
-        baseURL: "https://jsonplaceholder.typicode.com/",
-    });
-
-    const response = await fakerApi.get("users");
-    const responseBody = await response.json();
-    person = responseBody[0];
+    await APIController.init();
+    person = await APIController.getUser();
 });
 
 test.beforeEach(async ({ page }) => {
